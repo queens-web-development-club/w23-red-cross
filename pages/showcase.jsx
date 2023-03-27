50c559f84ee5ab81424ccbab30ff2e8d91c5fbae
Saul W <johndoe@example.com>
2023-03-26 22:27:25 -0400

    starting showcase page

--- pages/showcase.jsx
+++ pages/showcase.jsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
+import styles from '../components/Showcase.module.css';
+import indiaText from "../public/showcase/indiaShowcase.jpg";
+
 export default () => {
 	return (
-		<div>
-			<h1 className='container mx-auto text-4xl text-red-500'>
-				Showcase here
-			</h1>
+		<div className='w-screen h-full bg-[#333333]'>
+				<div className='h-full w-fit py-3 px-9 mx-auto font-bold text-3xl bg-[#CC3333] text-[#FBFBFB]'>
+					Showcase
+				</div>
+					<div className = {styles.section}>
+						<Image src={indiaText} className={styles.image}/>
+					</div>
 		</div>
 	);
 };
